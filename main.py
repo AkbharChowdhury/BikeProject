49c9b21cc6e272a04ae0e32ce7ba4b7c3eacd893
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-05-24 07:54:00 +0100

    a

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import json
+import ijson
+
 from classes import User
 
 
 def fetch_bike():
-    with open('bike.json') as f:
-        return json.load(f)
+    with open("bike.json", "rb") as f:
+        bike = {}
+        for field, data in ijson.kvitems(f, ""):
+            bike[field] = data
+    return bike
 
 
 def main():
